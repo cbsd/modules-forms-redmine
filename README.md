9905a784f166183de2abf4ddc76eb4cdf4e21c0c
olevole <olevole@srv-01.convectix.com>
2022-12-30 19:22:14 +0300

    wip

---
+++ README.md
@@ -0,0 +1,9 @@
+# modules-forms-redmine
+CBSD module to manage redmine
+
+Info: this module uses the CBSD puppet module
+
+To install:
+
+  - cbsd module mode=install forms-redmine
+
---
+++ initforms.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+pgm="${0##*/}"		# Program basename
+progdir="${0%/*}"	# Program directory
+: ${REALPATH_CMD=$( which realpath )}
+: ${SQLITE3_CMD=$( which sqlite3 )}
+: ${RM_CMD=$( which rm )}
+: ${MKDIR_CMD=$( which mkdir )}
+: ${FORM_PATH="/opt/forms"}
+: ${distdir="/usr/local/cbsd"}
+
+MY_PATH="$( ${REALPATH_CMD} ${progdir} )"
+HELPER="redmine"
+
+# MAIN
+if [ -z "${workdir}" ]; then
+	[ -z "${cbsd_workdir}" ] && . /etc/rc.conf
+	[ -z "${cbsd_workdir}" ] && exit 0
+	workdir="${cbsd_workdir}"
+fi
+
+set -e
+. ${distdir}/cbsd.conf
+. ${subrdir}/tools.subr
+. ${subr}
+set +e
+
+FORM_PATH="${workdir}/formfile"
+
+[ ! -d "${FORM_PATH}" ] && err 1 "No such ${FORM_PATH}"
+[ -f "${FORM_PATH}/${HELPER}.sqlite" ] && ${RM_CMD} -f "${FORM_PATH}/${HELPER}.sqlite"
+
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms.schema forms
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms.schema additional_cfg
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms_system.schema system
+
+# not neccesary here
+#INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,113,"skip_networking","skip_networking",'false','false','',1, "maxlen=60", "radio", "skip_networking_yesno", "" );
+
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,1,"-Globals","Globals",'Globals','PP','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,2,"timezone","System timezone",'Europe/Moscow','Europe/Moscow','',1, "maxlen=60", "inputbox", "", "" );
+
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,100,"-MySQL","MySQL",'MySQL','PP','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,102,"mysql_ver","MySQL version, MYSQL_DEFAULT in bsd.default-versions.mk",'57','57','',1, "maxlen=5", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,103,"-Additional","Additional params",'Additional params','','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,104,"bind_address","bind_address",'127.0.0.1','127.0.0.1','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,105,"expire_logs_days","expire_logs_days",'10','10','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,106,"key_buffer_size","key_buffer_size",'16M','16M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,107,"max_allowed_packet","max_allowed_packet",'16M','16M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,108,"max_binlog_size","max_binlog_size",'100M','100M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,109,"max_connections","max_connections",'151','151','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,110,"port","port",'3306','3306','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,114,"socket","socket",'/tmp/mysql.sock','/tmp/mysql.sock','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,115,"sort_buffer_size","sort_buffer_size",'8M','8M','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,116,"thread_cache_size","thread_cache_size",'8','8','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,117,"thread_stack","thread_stack",'256K','256K','',1, "maxlen=6", "inputbox", "", "" );
+
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,200,"-REDMINE","REDMINE",'REDMINE','PP','',1, "maxlen=60", "delimer", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,201,"db_password","Redmine user DB password",'redminepass','redminepass','',1, "maxlen=60", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,203,"redmine_package","redmine_package name",'redmine50','redmine50','',1, "maxlen=6", "inputbox", "", "" );
+INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,204,"redmine_port","redmine_port HTTP thin server",'3000','3000','',1, "maxlen=6", "inputbox", "", "" );
+COMMIT;
+EOF
+
+
+# yesno
+/usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms_yesno.schema skip_networking_yesno
+# Put boolean for use_sasl_yesno
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO skip_networking_yesno ( text, order_id ) VALUES ( "true", 1 );
+INSERT INTO skip_networking_yesno ( text, order_id ) VALUES ( "false", 0 );
+COMMIT;
+EOF
+
+
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+INSERT INTO system ( helpername, version, packages, have_restart ) VALUES ( "redmine", "201607", "www/redmine50", "redmine" );
+COMMIT;
+EOF
+
+# long description
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+BEGIN TRANSACTION;
+UPDATE system SET longdesc='\\
+Redmine is a flexible project management web application \\
+written using Ruby on Rails framework, it is cross-platform \\
+and cross-database. \\
+ \\
+Feature Overview: \\
+* Multiple projects support \\
+* Flexible role based access control \\
+* Flexible issue tracking system \\
+* Gantt chart and calendar \\
+* News, documents & files management \\
+* Feeds & email notifications \\
+* Per project wiki \\
+* Per project forums \\
+* Time tracking \\
+* Custom fields for issues, time-entries, projects and users \\
+* SCM integration (SVN, CVS, Git, Mercurial, Bazaar and Darcs) \\
+* Issue creation via email \\
+* Multiple LDAP authentication support \\
+* User self-registration support \\
+* Multilanguage support \\
+* Multiple databases support \\
+ \\
+WWW: https://www.redmine.org \\
+';
+COMMIT;
+EOF
